f51cbc5b2724ed5c4a5e109d0d4bce3598095d8a
Vu Phuc Tho <vuphuctho93@gmail.com>
2017-02-23 01:30:43 +0800

    add server and db

--- js/index.js
+++ js/index.js
@@ -5,83 +5,82 @@ var Navbar = require("./components/Navbar.js");
 var AddPostModal = require('./components/AddPostModal.js');
 
 const Datastore = require('nedb'), 
-	PostDB = new Datastore();
+    PostDB = new Datastore();
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleChildRequest = this.handleChildRequest.bind(this);
-		this.handleDbRequest = this.handleDbRequest.bind(this);
-		this.handleDisplayRequest = this.handleDisplayRequest.bind(this);
-		this.state = {
-			modalOpeningStates: {
-				AddPostModal: false
-			}
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.handleChildRequest = this.handleChildRequest.bind(this);
+        this.handleDbRequest = this.handleDbRequest.bind(this);
+        this.handleDisplayRequest = this.handleDisplayRequest.bind(this);
+        this.state = {
+            modalOpeningStates: {
+                AddPostModal: false
+            }
+        };
+    }
 
-	handleChildRequest(request) {
-		console.log(request);
-		switch (request.requestType) {
-			case 'db':
-				this.handleDbRequest(request);
-				break;
-			case 'display':
-				this.handleDisplayRequest(request);
-				break;
-			default:
-				break;
-		}
-	}
+    handleChildRequest(request) {
+        switch (request.requestType) {
+            case 'db':
+                this.handleDbRequest(request);
+                break;
+            case 'display':
+                this.handleDisplayRequest(request);
+                break;
+            default:
+                break;
+        }
+    }
 
-	handleDbRequest(request) {
-		switch(request.dbTarget) {
-			case 'post':
-				switch(request.action) {
-					case 'insert':
-						// PostDB.
-						console.log("here handle insert");
-						break;
-					default:
-						break;
-				}
-				break;
-			default:
-				break;
-		}
-	}	
+    handleDbRequest(request) {
+        switch(request.dbTarget) {
+            case 'post':
+                switch(request.action) {
+                    case 'insert':
+                        // PostDB.
+                        console.log("here handle insert");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+    }   
 
-	handleDisplayRequest(request) {
-		switch(request.action) {
-			case "openModal":
-				switch(request.displayTarget) {
-					case 'AddPostModal':
-						this.setState({
-							modalOpeningStates: {
-								AddPostModal: true
-							}
-						})
-						break;
-					default:
-						break;
-				}
-				break;
-			default:
-				break;
-		}
-	}
+    handleDisplayRequest(request) {
+        switch(request.action) {
+            case "openModal":
+                switch(request.displayTarget) {
+                    case 'AddPostModal':
+                        this.setState({
+                            modalOpeningStates: {
+                                AddPostModal: true
+                            }
+                        })
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
-	render() {
-		return (
-			<div className="container-fluid">
-				<Navbar request={this.handleChildRequest} />
-				<AddPostModal showModal={this.state.modalOpeningStates.AddPostModal} request={this.handleChildRequest} />
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="container-fluid">
+                <Navbar request={this.handleChildRequest} />
+                <AddPostModal showModal={this.state.modalOpeningStates.AddPostModal} request={this.handleChildRequest} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
